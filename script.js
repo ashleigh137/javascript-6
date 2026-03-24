41df191eae6158f2ecad427cca37f3a0b4e70bed
ashleigh137 <ashleigh137886@gmail.com>
2026-03-24 16:12:55 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ else
 msgBox.innerHTML = message;
 }
 
+function myFunction() {
+  document.getElementById("result").innerHTML =
+  document.getElementById("demo").firstChild.nodeValue;
+  document.getElementById("result1").innerHTML =
+  document.getElementById("demo").childNodes[0].nodeValue;
+}
+
 // Run validate when login is clicked
 document.getElementById("loginForm").onsubmit = validate;
 
